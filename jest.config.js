e9af854c0916863ed135bb5c9db1444d5e3c36ed
Matheus-fdte <matheus.barbieri@fdte.io>
2020-04-06 18:27:44 -0300

    remove global setup

--- jest.config.js
+++ jest.config.js
@@ -52,7 +52,7 @@ module.exports = {
   // forceCoverageMatch: [],
 
   // A path to a module which exports an async function that is triggered once before all test suites
-  globalSetup: './settings/tests/setup.js',
+  // globalSetup: './settings/tests/setup.js',
 
   // A path to a module which exports an async function that is triggered once after all test suites
   // globalTeardown: null,
