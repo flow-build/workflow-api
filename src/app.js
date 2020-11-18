4811235685f11905dfc154283eeeae32a17f8a03
Gabriel Lopes Rodrigues <gabriellopes@usp.br>
2020-11-18 19:19:39 -0300

    Added OpenAPI3, with only a few routes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,18 @@ const Koa = require("koa");
 const router = require("./router");
 const cors = require('koa2-cors');
 const jwt = require("koa-jwt");
+const swagger = require("swagger2");
+const { ui } = require("swagger2-koa");
+
 const { setEngine, getEngine, setCockpit, getCockpit } = require("./engine");
 const { Engine, Cockpit } = require("@flowbuild/engine");
 const { db } = require("./tests/utils/db");
 
 const { jwtSecret } = require("./utils/jwt_secret");
+
+// At execution time, the current folder is the root, that is why src is needed
+const swaggerDocument = swagger.loadDocumentSync("src/openapi3.yaml");
+
 
 const startServer = (port) => {
   let engine = getEngine();
@@ -33,6 +40,7 @@ const startServer = (port) => {
       jwt({ secret: jwtSecret, debug: true })
     ]
   }).routes());
+  app.use(ui(swaggerDocument, "/swagger"))
   return app.listen(port, function () {
     console.log("Server running")
   });
