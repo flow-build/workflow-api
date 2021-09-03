656a049eb096a9cbb8031832903f8f488013ad6b
Gustavo Haramura <gustavo.haramura@fdte.io>
2021-09-03 18:36:09 -0300

    perf:

---
+++ db/migrations/20210510165700_add_blueprint_hash_to_workflow.js
@@ -0,0 +1,12 @@
+
+exports.up = function (knex) {
+    return knex.schema.table('workflow', table => {
+        table.text('blueprint_hash')
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.table('workflow', table => {
+        table.dropColumn('blueprint_hash');
+    });
+};
