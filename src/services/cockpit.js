6b1602c0173c33669ae3c002b05ff7c3ac7dd0bf
Gustavo Haramura <gustavo.haramura@fdte.io>
2021-09-09 11:52:26 -0300

    refactor(services): :truck: rename services

---
+++ src/services/cockpit.js
@@ -0,0 +1,142 @@
+let db;
+
+function setDbConnection(dbConnection) {
+  db = dbConnection;
+}
+
+async function fetchWorkflowByName(workflow_name) {
+    
+    let result;
+  
+    result = await db("workflow")
+    .where("name", workflow_name)
+    .orderBy("created_at", "desc")
+    
+    return result;
+}
+
+async function fetchWorkflowProcessesByWorkflowName(workflow_name, status = null , limit = 20 , offset = 0 ) {
+    
+    let result;
+  
+    let query = await db("process")
+    .select('process.id','process.created_at','process.current_state_id','process.current_status','workflow.name','workflow.version')
+    .join("workflow", "workflow.id", "process.workflow_id")
+    .where("workflow.name", workflow_name)
+    .orderBy("process.created_at", "desc")
+    .limit(limit)
+    .offset(offset);
+
+    if (status && typeof status === "object") {
+        query.whereIn("process.current_status", status);
+    }
+
+    result = await query;
+
+    return result;
+}
+
+async function fetchWorkflowProcessesByWorkflowId(workflow_id, status = null , limit = 20 , offset = 0 ) {
+    
+    let result;
+  
+    let query = await db("process")
+    .select('process.id','process.created_at','process.current_state_id','process.current_status','workflow.name','workflow.version')
+    .join("workflow", "workflow.id", "process.workflow_id")
+    .where("workflow.id", workflow_id)
+    .orderBy("process.created_at", "desc")
+    .limit(limit)
+    .offset(offset);
+
+    if (status && typeof status === "object") {
+        query.whereIn("process.current_status", status);
+    }
+
+    result = await query;
+
+    return result;
+}
+
+async function fetchProcessExecution(process_id) {
+    let result;
+
+    result = await db("process_state")
+    .select('id','step_number','node_id','next_node_id','status')
+    .where('process_id',process_id)
+    .orderBy('step_number','desc')
+
+    return result;
+}
+
+async function fetchProcessState(state_id) {
+    let result;
+
+    result = await db("process_state")
+    .where('id',state_id)
+    .first()
+
+    return result;
+}
+
+async function fetchProcessStateByNodeId(process_id, node_id) {
+    let result;
+
+    result = await db("process_state")
+    .select(
+        'workflow.name as workflow_name',
+        'workflow.version',
+        'workflow.id as workflow_id',
+        'process_state.id as state_id',
+        'process_state.step_number',
+        'process_state.next_node_id',
+        'process_state.result',
+        'process_state.status',
+        'process_state.created_at'
+    )
+    .join("process", "process.id", "process_state.process_id")
+    .join("workflow", "workflow.id", "process.workflow_id")
+    .where({
+        process_id: process_id,
+        node_id: node_id
+    })
+    .orderBy('step_number','desc')
+
+    return result;
+}
+
+async function fetchStatesByNodeId(workflow_name, node_id, filters=null) {
+    let result;
+
+    //TODO: add filters for date, status
+
+    result = await db("process_state")
+    .select(
+        'workflow.name as workflow_name',
+        'workflow.version',
+        'workflow.id as workflow_id',
+        'process_state.id as state_id',
+        'process_state.step_number',
+        'process_state.next_node_id',
+        'process_state.result',
+        'process_state.status',
+        'process_state.created_at'
+    )
+    .join("process", "process.id", "process_state.process_id")
+    .join("workflow", "workflow.id", "process.workflow_id")
+    .where("process_state.node_id", node_id)
+    .where("workflow.name",workflow_name)
+    .orderBy('created_at','desc')
+
+    return result;
+}
+
+module.exports = {
+    setDbConnection,
+    fetchWorkflowByName,
+    fetchWorkflowProcessesByWorkflowName,
+    fetchWorkflowProcessesByWorkflowId,
+    fetchProcessExecution,
+    fetchProcessState,
+    fetchProcessStateByNodeId,
+    fetchStatesByNodeId
+}
