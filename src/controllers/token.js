bb4b89655e8429b083167330bffbe1a6ae0ae1f1
Gustavo Haramura <gustavo.haramura@fdte.io>
2021-10-14 16:35:01 -0300

    feat: convert claims to array

--- src/controllers/token.js
+++ src/controllers/token.js
@@ -1,4 +1,5 @@
 const { v1: uuid } = require("uuid");
+const { nanoid } = require("nanoid");
 const { createJWTToken } = require("../services/tokenGenerator");
 const { jwtSecret } = require("../utils/jwtSecret");
 const { logger } = require("../utils/logger");
@@ -16,7 +17,13 @@ const getToken = (ctx, next) => {
   if (!body?.claims) {
     logger.debug("Set an empty claims list");
     body.claims = [];
+  } else if (!Array.isArray(body.claims)) {
+    let claims = [];
+    claims.push(body.claims);
+    body.claims = claims;
   }
+
+  body.session_id = nanoid();
 
   const jwtToken = createJWTToken(body, secret, duration);
   ctx.status = 200;
