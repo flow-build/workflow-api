6830506cff330aa1bef872a6c763ea979ae560f3
Gustavo Haramura <gustavo.haramura@fdte.io>
2021-10-27 08:51:42 -0300

    build(knex): change pool settings

--- knexfile.js
+++ knexfile.js
@@ -1,37 +1,37 @@
-const path = require('path');
-const BASE_PATH = path.join(__dirname, 'db');
+const path = require("path");
+const BASE_PATH = path.join(__dirname, "db");
 
 module.exports = {
   test: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      host: process.env.POSTGRES_HOST || '0.0.0.0',
-      port: process.env.POSTGRES_PORT || '5432',
-      user: process.env.POSTGRES_USER || 'postgres',
-      password: process.env.POSTGRES_PASSWORD || 'postgres',
-      database: process.env.POSTGRES_DATABASE || 'workflow'
+      host: process.env.POSTGRES_HOST || "0.0.0.0",
+      port: process.env.POSTGRES_PORT || "5432",
+      user: process.env.POSTGRES_USER || "postgres",
+      password: process.env.POSTGRES_PASSWORD || "postgres",
+      database: process.env.POSTGRES_DATABASE || "workflow",
     },
     migrations: {
-      directory: path.join(BASE_PATH, 'migrations')
+      directory: path.join(BASE_PATH, "migrations"),
     },
     seeds: {
-      directory: path.join(BASE_PATH, 'seeds')
-    }
+      directory: path.join(BASE_PATH, "seeds"),
+    },
   },
   docker: {
-    client: 'pg',
+    client: "pg",
     connection: {
       host: "flowbuild_db",
       user: "postgres",
       password: "postgres",
-      database: "workflow"
+      database: "workflow",
     },
     migrations: {
-      directory: path.join(BASE_PATH, 'migrations')
+      directory: path.join(BASE_PATH, "migrations"),
     },
     seeds: {
-      directory: path.join(BASE_PATH, 'seeds')
-    }
+      directory: path.join(BASE_PATH, "seeds"),
+    },
   },
   dockerLocal: {
     client: "pg",
@@ -42,7 +42,12 @@ module.exports = {
       database: "workflow",
       port: 5432,
     },
-    pool: { min: 10, max: 40 },
+    pool: {
+      min: 0,
+      max: 40,
+      acquireTimeoutMillis: 60000,
+      idleTimeoutMillis: 600000,
+    },
     migrations: {
       directory: path.join(BASE_PATH, "migrations"),
     },
@@ -50,4 +55,4 @@ module.exports = {
       directory: path.join(BASE_PATH, "seeds"),
     },
   },
-}
+};
