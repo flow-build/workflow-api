3237b08be181cd499524c7777f657cf5846b103c
Gustavo Haramura <gustavo.haramura@fdte.io>
2022-09-17 19:02:35 -0300

    fix: activity manager tests

--- src/tests/activityManager.test.js
+++ src/tests/activityManager.test.js
@@ -6,7 +6,6 @@ const { startServer } = require("../app");
 const workflowSamples = require("../samples/workflows");
 const { delay, cleanDb } = require("./utils/auxiliar");
 const { config } = require("./utils/requestConfig");
-
 const { setEngine, setCockpit } = require("../engine");
 const { Engine, Cockpit } = require("@flowbuild/engine");
 const engine = new Engine("knex", db);
@@ -18,13 +17,14 @@ let server;
 const prefix = "/activity_manager";
 
 beforeAll(async () => {
+  process.env.ENGINE_HEARTBEAT=false
   server = startServer(3001);
   axios.defaults.baseURL = config.baseURL;
   axios.defaults.headers = config.headers;
   axios.defaults.validateStatus = config.validateStatus;
+  jest.setTimeout(30000);
 
   await cleanDb();
-
   await axios.post("/workflows", workflowSamples.singleUserTask);
 });
 
@@ -33,10 +33,9 @@ beforeEach(async () => {
 });
 
 afterAll(async () => {
-  await cleanDb();
-  await db.destroy();
-  Engine.kill();
-  await server.close();
+  cleanDb();
+  db.destroy();
+  server.close();
 });
 
 describe("POST /:id/commit", () => {
@@ -45,38 +44,35 @@ describe("POST /:id/commit", () => {
 
   beforeEach(async () => {
     //INICIAR UM PROCESSO E GUARDAR O ID DO PROCESSO
-    const process = await axios.post(
-      `/workflows/name/${workflowSamples.singleUserTask.name}/start`,
-      {}
-    );
-    await delay(500);
+    const process = await axios.post(`/workflows/name/${workflowSamples.singleUserTask.name}/start`, {});
+    await delay(1500);
     processId = process.data.process_id;
     //OBTER O ID DO ACTIVITY_MANAGER
     const activityManager = await axios.get(`/processes/${processId}/activity`);
+    console.log(`AMID ${activityManager.data.id}`);
     activityManagerId = activityManager.data.id;
   });
 
   test("should return 200 for existing id and should not affect the process status", async () => {
-    const commitCall = await axios.post(
-      `${prefix}/${activityManagerId}/commit`
-    );
+    console.log("should return 200 for existing id and should not affect the process status");
+    const commitCall = await axios.post(`${prefix}/${activityManagerId}/commit`);
     expect(commitCall.status).toBe(200);
 
     const response = await axios.get(`/processes/${processId}/state`);
-    expect(response.data.current_status).toBe("waiting");
+    expect(response.data.state.status).toBe("waiting");
   });
 
   test("should keep returning 200 for an existing id previous committed", async () => {
+    console.log("should keep returning 200 for an existing id previous committed");
     const firstCall = await axios.post(`${prefix}/${activityManagerId}/commit`);
     expect(firstCall.status).toBe(200);
 
-    const secondCall = await axios.post(
-      `${prefix}/${activityManagerId}/commit`
-    );
+    const secondCall = await axios.post(`${prefix}/${activityManagerId}/commit`);
     expect(secondCall.status).toBe(200);
   });
 
   test("should return 404 for an random non-existing id", async () => {
+    console.log("should return 404 for an random non-existing id");
     const randomId = uuid();
     const commitCall = await axios.post(`${prefix}/${randomId}/commit`);
     expect(commitCall.status).toBe(404);
@@ -88,44 +84,42 @@ describe("POST /:id/submit", () => {
 
   beforeEach(async () => {
     //INICIAR UM PROCESSO E GUARDAR O ID DO PROCESSO
-    const process = await axios.post(
-      `/workflows/name/${workflowSamples.singleUserTask.name}/start`,
-      {}
-    );
-    await delay(500);
+    const process = await axios.post(`/workflows/name/${workflowSamples.singleUserTask.name}/start`, {});
+    await delay(1500);
     processId = process.data.process_id;
     //OBTER O ID DO ACTIVITY_MANAGER
     const activityManager = await axios.get(`/processes/${processId}/activity`);
+    console.log(`AMID ${activityManager.data.id}`);
     activityManagerId = activityManager.data.id;
   });
 
   test("should return 202 for existing id and should affect the process status", async () => {
-    const submitCall = await axios.post(
-      `${prefix}/${activityManagerId}/submit`
-    );
+    console.log("should return 202 for existing id and should affect the process status");
+    const submitCall = await axios.post(`${prefix}/${activityManagerId}/submit`);
     expect(submitCall.status).toBe(202);
-    await delay(500);
+    await delay(1500);
     const response = await axios.get(`/processes/${processId}/state`);
-    expect(response.data.current_status).toBe("finished");
+    expect(response.data.state.status).toBe("finished");
   });
 
   test("should return 422 for an existing id already submitted", async () => {
+    console.log("should return 422 for an existing id already submitted");
     const firstCall = await axios.post(`${prefix}/${activityManagerId}/submit`);
     expect(firstCall.status).toBe(202);
 
-    const secondCall = await axios.post(
-      `${prefix}/${activityManagerId}/submit`
-    );
+    const secondCall = await axios.post(`${prefix}/${activityManagerId}/submit`);
     expect(secondCall.status).toBe(422);
   });
 
   test("should return 422 for an id from a interrupted process", async () => {
+    console.log("should return 422 for an id from a interrupted process");
     await axios.post(`/processes/${processId}/abort`);
     const firstCall = await axios.post(`${prefix}/${activityManagerId}/submit`);
     expect(firstCall.status).toBe(422);
   });
 
   test("should return 404 for an random non-existing id", async () => {
+    console.log("should return 404 for an random non-existing id");
     const randomId = uuid();
     const commitCall = await axios.post(`${prefix}/${randomId}/submit`);
     expect(commitCall.status).toBe(404);
