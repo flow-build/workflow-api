e061043267a36a2e3fda4adc941aa2bef051e549
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-12-05 12:08:36 -0300

    feat: add new validationExecutionData method

--- src/nodes/remapDataNode.js
+++ src/nodes/remapDataNode.js
@@ -46,10 +46,11 @@ class RemapDataNode extends Nodes.SystemTaskNode {
     };
   }
 
-  static validate(spec) {
+  static validate(spec, schema = null) {
     const ajv = new Ajv({ allErrors: true });
     addFormats(ajv);
-    const validate = ajv.compile(RemapDataNode.schema);
+    const validationSchema = schema || RemapDataNode.schema;
+    const validate = ajv.compile(validationSchema);
     const validation = validate(spec);
     return [validation, JSON.stringify(validate.errors)];
   }
@@ -58,56 +59,73 @@ class RemapDataNode extends Nodes.SystemTaskNode {
     return RemapDataNode.validate(this._spec);
   }
 
+  static validateExecutionData(spec) {
+    const schema = {
+      type: "object",
+      required: ["data", "dictionary"],
+      properties: {
+        data: { type: "array", items: { type: "object" } },
+        dictionary: { type: "object" }
+      }
+    }
+    return RemapDataNode.validate(spec, schema);
+  }
+
   async _run(executionData) {
     try {
       logger.debug("remapData Node running");
-      const { data, dictionary  } = executionData;
-      let status = 'success';
-      let messages = [];
-
-      let remapped_data = data.map((item) => {
-        const remapped_item = {};
-        for (const [key, value] of Object.entries(dictionary)) {
-          if (typeof value === 'string' && value.length > 0 && value.includes('.')) {
-            const object_value = _.get(item, value);
-            if (object_value !== undefined) {
-              remapped_item[key] = object_value;
-            }
-          } else if (value !== null && value.length > 0 && item[value] !== undefined) {
-            remapped_item[key] = item[value];
-          } else if (value === null || value.length === 0) {
-            remapped_item[key] = value;
-          }
-
-          if (remapped_item[key] === undefined) {
-            messages.push(`'${value}' from dictionary not found in data`);
-          }
-        }
-        return remapped_item;
-      });
-
-      if (messages.length > 0) {
-        if (Object.keys(remapped_data[0]).length > 0) {
-          status = 'warning';
-        } else {
-          status = 'error';
-          remapped_data = [];
-        }
+      const [is_valid, validation_errors] = RemapDataNode.validateExecutionData(executionData);
+      if(!is_valid) {
+        const errors = JSON.parse(validation_errors).map((err) => `field '${err.instancePath}' ${err.message}`);
+        throw JSON.stringify(errors);
       }
+      const { data, dictionary } = executionData;
+        let status = 'success';
+        let messages =[];
+
+        let remapped_data = data.map((item) => {
+          const remapped_item = {};
+          for (const [key, value] of Object.entries(dictionary)) {
+            if (typeof value === 'string' && value.length > 0 && value.includes('.')) {
+              const object_value = _.get(item, value);
+              if (object_value !== undefined) {
+                remapped_item[key] = object_value;
+              }
+            } else if (value !== null && value.length > 0 && item[value] !== undefined) {
+              remapped_item[key] = item[value];
+            } else if (value === null || value.length === 0) {
+              remapped_item[key] = value;
+            }
+
+            if (remapped_item[key] === undefined) {
+              messages.push(`'${value}' from dictionary not found in data`);
+            }
+          }
+          return remapped_item;
+        });
+
+        if(messages.length > 0) {
+          if (Object.keys(remapped_data[0]).length > 0) {
+        status = 'warning';
+      } else {
+        status = 'error';
+        remapped_data = [];
+      }
+    }
 
       return [
-        {
-          status,
-          messages,
-          data: remapped_data
-        },
-        ProcessStatus.RUNNING,
-      ];
-    } catch (err) {
-      logger.error("remapData Node failed", err);
-      throw new Error(err);
-    }
+      {
+        status,
+        messages,
+        data: remapped_data
+      },
+      ProcessStatus.RUNNING,
+    ];
+  } catch(err) {
+    logger.error("remapData Node failed", err);
+    throw new Error(err);
   }
+}
 }
 
 module.exports = RemapDataNode;
