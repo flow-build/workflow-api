881246cb650f4d28b0fda01d2f0b6301ee9dbd8c
Gustavo Haramura <gustavo.haramura@fdte.io>
2023-02-17 11:19:18 -0300

    feat: :sparkles: allow to set diferent jwt key, algorithm and payload structure

--- src/middlewares/actordata.js
+++ src/middlewares/actordata.js
@@ -1,4 +1,41 @@
 const { logger } = require("../utils/logger");
+const _ = require('lodash')
+
+const mapDefaultData = (ctx) => {
+  const customMap = {
+    actor_id: process.env.JWT_PATH_ACTOR_ID || 'actor_id',
+    claims: process.env.JWT_PATH_CLAIMS || 'claims',
+    session_id: process.env.JWT_PATH_SESSION_ID  || 'session_id',
+  }
+
+  const actor_id = _.get(ctx.state.user, customMap.actor_id);
+  const claims = _.get(ctx.state.user, customMap.claims);
+  const session_id = _.get(ctx.state.user, customMap.session_id);
+  const trace = {
+    tracestate: ctx.request.headers.tracestate,
+    traceparent: ctx.request.headers.traceparent,
+  }; 
+
+  return { actor_id, claims, session_id, trace }
+}
+
+const mapExtraData = (user) => {
+  if(!process.env.JWT_EXTRA_KEYS) {
+    return
+  }
+
+  const keys = process.env.JWT_EXTRA_KEYS.split(',')
+  let extData = {}
+  if(keys && keys.length > 0) {
+    for (const key of keys) {  
+      if(user[key]) {
+        extData[key] = user[key]
+      }
+    }
+  }
+
+  return extData
+}
 
 const captureActorData = async (ctx, next) => {
   logger.debug("captureActorData");
@@ -7,42 +44,37 @@ const captureActorData = async (ctx, next) => {
     ctx.status = 401;
     ctx.body = {
       message: "User data not found",
+      error: ctx.state.jwtOriginalError
     };
     return;
-  }
-  const actor_id = ctx.state.user.actor_id;
+  }  
 
-  if (!actor_id) {
+  const defaultData = mapDefaultData(ctx)
+
+  if (!defaultData.actor_id) {
     logger.debug("no actor_id");
     ctx.status = 401;
     ctx.body = { message: "Actor id not found" };
     return;
   }
-
-  const claims = ctx.state.user.claims;
-  if (!Array.isArray(claims)) {
+  
+  if (!Array.isArray(defaultData.claims)) {
     logger.debug("invalid claims");
     ctx.status = 401;
     ctx.body = { message: "Invalid claims" };
     return;
   }
 
-  let trace;
-  trace = {
-    tracestate: ctx.request.headers.tracestate,
-    traceparent: ctx.request.headers.traceparent,
-  };
-
-  const session_id = ctx.state.user.session_id;
-
   if (ctx.state.actor_data) {
-    ctx.state.actor_data["actor_id"] = actor_id;
-    ctx.state.actor_data["claims"] = claims;
-    ctx.state.actor_data.trace = trace;
-    ctx.state.actor_data["session_id"] = session_id;
+    ctx.state.actor_data["actor_id"] = defaultData.actor_id;
+    ctx.state.actor_data["claims"] = defaultData.claims;
+    ctx.state.actor_data.trace = defaultData.trace;
+    ctx.state.actor_data["session_id"] = defaultData.session_id;
   } else {
-    ctx.state.actor_data = { trace, actor_id, claims, session_id };
+    ctx.state.actor_data = defaultData;
   }
+
+  ctx.state.actor_data.extData = mapExtraData(ctx.state.user);
 
   return next();
 };
