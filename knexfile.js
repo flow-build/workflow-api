68c781cab76a6f62343ab7e28e254470d3784598
Gustavo Haramura <gustavo.haramura@fdte.io>
2023-03-25 11:53:51 -0300

    fix: :card_file_box: add ssl property to prod knex config

--- knexfile.js
+++ knexfile.js
@@ -64,6 +64,7 @@ module.exports = {
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
+      ssl: { rejectUnauthorized: false },
     },
     migrations: {
       directory: path.join(BASE_PATH, "migrations"),
