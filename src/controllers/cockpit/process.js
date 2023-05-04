95e89d74e31692f4605c93910fd683402d9e25a4
imagure <ricardo.imagure@gmail.com>
2023-05-04 18:19:14 -0300

    fix: removes export from unexisting name

--- src/controllers/cockpit/process.js
+++ src/controllers/cockpit/process.js
@@ -275,7 +275,6 @@ const expireProcess = async (ctx, next) => {
 module.exports = {
   getProcessesByWorkflowId,
   getProcessesByWorkflowName,
-  getProcessExecution,
   getProcessStateExecutionHistory,
   getStatesFromNode,
   setProcessState,
